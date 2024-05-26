dbbd19ef3e330b202aa8b3e92218796f31216e3b
Manoj D More <manoj.contactpoint@gmail.com>
2024-05-26 23:26:28 +0530

    updated

--- tfe/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "AWS_ACCESS_KEY_ID" {
-	description = "This aws cli key is added in the HCP terraform variable"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-	description = "This aws cli key value is added in the HCP terraform variable"
-}
