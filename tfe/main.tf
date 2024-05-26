58b6b47d1966469358fa02e59379b7a587e84de6
Manoj D More <manoj.contactpoint@gmail.com>
2024-05-26 22:44:00 +0530

    removed HCP terraform cloud config

--- tfe/main.tf
+++ tfe/main.tf
@@ -1,10 +1,5 @@
 terraform {
-  cloud {
-    organization = "manoj-organization"
-    workspaces {
-      name = "manoj-terraform-aws"
-    }
-  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
